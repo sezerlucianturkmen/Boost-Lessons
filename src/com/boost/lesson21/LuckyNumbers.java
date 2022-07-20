aec2cf0a954dfd45fa5624e401a32c04d9eb0272
Mandarina <Mandarina@LAPTOP-I1SFUF5H>
2022-07-20 19:11:18 +0300

    update lesson and learn

--- src/com/boost/lesson21/LuckyNumbers.java
+++ src/com/boost/lesson21/LuckyNumbers.java
@@ -3,22 +3,22 @@ package com.boost.lesson21;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class LuckyNumbers {
 
 	public static final int maxNumber = 100;
 	public static final int quantityNumber = 10000;
 
-	Map<Integer, Integer> luckyNumbers;
+	Map<Integer, Integer> numbersMap;
 	List<Integer> numberList;
-	Set<Integer> setNumbers;
+	Set<Integer> luckyNumbers;
 
 	public static void main(String[] args) {
 
@@ -28,50 +28,43 @@ public class LuckyNumbers {
 		luckyNumbers.makeSet();
 
 		System.out.println("Numbers >50");
-		luckyNumbers.setNumbers.stream().filter(x -> x > 50).forEach(System.out::println);
+		luckyNumbers.luckyNumbers.stream().filter(x -> x > 50).forEach(System.out::println);
 		System.out.println("Sum of numbers");
-		System.out.println(luckyNumbers.setNumbers.stream().reduce((s1, s2) -> s1 + s2).get());
+		System.out.println(luckyNumbers.luckyNumbers.stream().reduce((s1, s2) -> s1 + s2).get());
 		System.out.println("sum of numbers>50");
-		Optional<Integer> sum = luckyNumbers.setNumbers.stream().filter(s -> s > 50)
+		Optional<Integer> sum = luckyNumbers.luckyNumbers.stream().filter(s -> s > 50)
 				.reduce((num1, num2) -> num1 + num2);
 		System.out.println(sum.get());
 	}
 
-	public Integer pickNumber() {
-		Random random = new Random();
-		Integer number = random.nextInt(1, 101);
-		return number;
-	}
-
-	public Integer pickNumber(int quantity) {
-		Random random = new Random();
-		Integer number = random.nextInt(quantity);
-		return number;
-	}
-
 	public LuckyNumbers() {
 		super();
-		this.luckyNumbers = new HashMap<Integer, Integer>();
+		this.numbersMap = new HashMap<Integer, Integer>();
 		this.numberList = new ArrayList<Integer>();
-		this.setNumbers = new HashSet<Integer>();
+		this.luckyNumbers = new TreeSet<Integer>();
+	}
+
+	public int pickNumber(int quantity) {
+		Random random = new Random();
+		return random.nextInt(quantity);
 	}
 
 	public void makeMap() {
-		for (int i = 0; i < 10000; i++) {
+		for (int i = 0; i < quantityNumber; i++) {
 
-			int number = pickNumber();
+			int number = pickNumber(maxNumber) + 1;
 
-			if (luckyNumbers.containsKey(number)) {
-				luckyNumbers.replace(number, luckyNumbers.get(number) + 1);
+			if (numbersMap.containsKey(number)) {
+				numbersMap.replace(number, numbersMap.get(number) + 1);
 			} else {
-				luckyNumbers.put(number, 1);
+				numbersMap.put(number, 1);
 			}
 		}
 		// luckyNumbers.entrySet().forEach(System.out::println);
 	}
 
 	public void addList() {
-		for (Entry<Integer, Integer> numbers : luckyNumbers.entrySet()) {
+		for (Entry<Integer, Integer> numbers : numbersMap.entrySet()) {
 			for (int i = 0; i < numbers.getValue(); i++) {
 				numberList.add(numbers.getKey());
 			}
@@ -84,9 +77,9 @@ public class LuckyNumbers {
 		while (luckyNumbers.size() < 10) {
 
 			int index = pickNumber(10);
-			setNumbers.add(numberList.get(index));
+			luckyNumbers.add(numberList.get(index));
 		}
-		setNumbers.forEach(System.out::println);
+		luckyNumbers.forEach(System.out::println);
 	}
 
 }
